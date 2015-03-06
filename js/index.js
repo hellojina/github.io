b9763b51c810f0e708708b5f0d304e1d7600b697
Jungil Han <jungil.han@gmail.com>
2015-03-07 00:40:48 +0900

    Added parallax scrolling

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 ﻿$(document).ready(function () {
+    $.stellar();
     $(".scroll").click(function (event) {
         event.preventDefault();
         $('html,body').animate({scrollTop: $(this.hash).offset().top}, 500);
@@ -73,6 +74,16 @@ $document.scroll(function () {
             $(".about_availability").fadeOut(300);
     }
 });
+
+(function() {
+    var mainTitle = $('.main_title');
+
+    $(document).scroll(function(e) {
+        var percent = $(document).scrollTop() / $(window).height();
+        percent = percent > 1 ? 1 : percent;
+        mainTitle.css('opacity', 1 - percent);
+    });
+}());
 
 $(window).resize(function () {
     if ($document.scrollTop() >= (windowHeight * 0.8)) {
